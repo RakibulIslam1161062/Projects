44e4585d98c67e66f24197f9aba6734a1891e826
Md. Rakibul Islam <36030906+RakibulIslam1161062@users.noreply.github.com>
2018-04-15 17:06:51 +1200

    Update ConvToBinary,block,resize.cpp

--- InputBinaryConvPadding/ConvToBinary,block,resize.cpp
+++ InputBinaryConvPadding/ConvToBinary,block,resize.cpp
@@ -100,6 +100,7 @@ vector<unsigned long> paddingZeros(vector<unsigned long> block)
 	int k=448-1-lengthOfString;
 	unsigned long pad1n70=0x10000000;
 	k=k-7;
+	
 	block.push_back(pad1n70);
 	//unsigned long o=0x00000000;
 	
@@ -142,6 +143,99 @@ vector<unsigned long> resizeInto16Blocks(vector<unsigned long>message)
 	
 	return resizedMsgBlock;
 }
+
+
+
+string computeTheHashValue(vector<unsigned long>msg)
+{
+	unsigned long k[64] = {
+		0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,
+		0x923f82a4,0xab1c5ed5,0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,
+		0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,0xe49b69c1,0xefbe4786,
+		0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
+		0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,
+		0x06ca6351,0x14292967,0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,
+		0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,0xa2bfe8a1,0xa81a664b,
+		0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
+		0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,
+		0x5b9cca4f,0x682e6ff3,0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,
+		0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+	};
+	
+	
+    unsigned long static H0 = 0x6a09e667;
+	unsigned long static H1 = 0xbb67ae85;
+	unsigned long static H2 = 0x3c6ef372;
+	unsigned long static H3 = 0xa54ff53a;
+	unsigned long static H4 = 0x510e527f;
+	unsigned long static H5 = 0x9b05688c;
+	unsigned long static H6 = 0x1f83d9ab;
+	unsigned long static H7 = 0x5be0cd19;
+	
+	unsigned long w[64];
+	
+	for(int t=0;t<=15;t++)
+		w[t]=msg[t] & 0xFFFFFFFF;
+	
+	
+	for(int t=16;t<64;t++)
+	{
+		w[t]= SIGMA1(w[t-2]) + w[t-7] + SIGMA0(w[t-15]) + w[t-16];
+		w[t]=w[t] & 0xFFFFFFFF;
+	}
+	
+	
+	//for(int t=0;t<64;t++) cout<<w[t];
+	//ekhan pojnto checked
+	unsigned long a = H0;
+	unsigned long b = H1;
+	unsigned long c = H2;
+	unsigned long d = H3;
+	unsigned long e = H4;
+	unsigned long f = H5;
+	unsigned long g = H6;
+	unsigned long h = H7;
+	
+	
+        unsigned long temp1;
+        unsigned long temp2;
+        int t;
+        for( t=0;t<64;t++) 
+        {
+
+		temp1= h + EPS1(e) + CH(e,f,g) + k[t] + w[t];
+	        //cout<<temp1;
+		//cout<<h;
+		//<<endl<<EPS1(e)<<endl<<CH(e,f,g)<<endl<<k[t-1]<<endl<<w[t-1]<<endl;	
+
+	        temp2= EPS0(a) + MAJ(a,b,c);
+
+		h = g;
+		g = f;
+		f = e;
+		e = (d + temp1) & 0xFFFFFFFF; 
+		d = c;
+		c = b;
+		b = a;
+		a = (temp1 + temp2) & 0xFFFFFFFF;
+	}
+	
+	
+	H0 = (H0 + a) & 0xFFFFFFFF;
+	H1 = (H1 + b) & 0xFFFFFFFF;
+	H2 = (H2 + c) & 0xFFFFFFFF;
+	H3 = (H3 + d) & 0xFFFFFFFF;
+	H4 = (H4 + e) & 0xFFFFFFFF;
+	H5 = (H5 + f) & 0xFFFFFFFF;
+	H6 = (H6 + g) & 0xFFFFFFFF;
+	H7 = (H7 + h) & 0xFFFFFFFF;
+
+	return show_as_hex(H0) + show_as_hex(H1) + show_as_hex(H2) +
+		   show_as_hex(H3) + show_as_hex(H4) + show_as_hex(H5) +
+		   show_as_hex(H6) + show_as_hex(H7);
+}
+
+
 
 int main()
 {
